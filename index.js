e2fecfd796e7362bc70ba59ec2a4bb194305a626
Danilov_Nikolai <dan1k91@mail.ru>
2024-06-17 00:22:34 +0300

    mongoDB connected

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import 'dotenv/config';
+// для работы с MongoDB
+import mongoose from 'mongoose';
 
+const PORT = process.env.PORT;
 const app = express();
 
-const PORT = 4444;
+// data base connection
+mongoose
+  .connect(
+    `mongodb+srv://Danilov:${process.env.DB_PASSWORD}@test-cluster.9rnehkm.mongodb.net/?retryWrites=true&w=majority&appName=Test-cluster`
+  )
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
 
+// routes
 app.get('/', (req, res) => console.log('Route test'));
 
 app.listen(PORT, (err) => {
