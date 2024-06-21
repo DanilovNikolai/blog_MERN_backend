37659166a2574c79d9d4e99287094da4f34aa82b
Danilov_Nikolai <dan1k91@mail.ru>
2024-06-21 23:55:50 +0300

    C:/Program Files/Git/tags route & getLastTags controller added

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -102,3 +102,19 @@ export const update = async (req, res) => {
     res.status(500).json({ message: 'Не удалось обновить статью' });
   }
 };
+
+export const getLastTags = async (req, res) => {
+  try {
+    // Находим первые 5 статей в БД
+    const posts = await PostModel.find().limit(5).exec();
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Не удалось получить тэги' });
+  }
+};
